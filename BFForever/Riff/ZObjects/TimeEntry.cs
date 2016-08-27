f868ff043c46f13429105eea293294e1a2e97589
PikminGuts92 <PikminGuts92@gmail.com>
2016-08-26 23:08:34 -0400

    Added abstract TimeEntry class.

---
+++ BFForever/Riff/ZObjects/TimeEntry.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BFForever.Riff
+{
+    public abstract class TimeEntry
+    {
+        private float _start;
+        private float _end;
+
+        /// <summary>
+        /// Abstract constructor for time entry.
+        /// </summary>
+        public TimeEntry()
+        {
+            Start = 0.0f;
+            End = 0.0f;
+        }
+
+        /// <summary>
+        /// Gets or sets start time (ms)
+        /// </summary>
+        public float Start
+        {
+            get { return _start; }
+            set
+            {
+                if (value >= 0.0f)
+                    _start = value;
+                else
+                    _start = 0.0f;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets end time (ms)
+        /// </summary>
+        public float End
+        {
+            get { return _end; }
+            set
+            {
+                if (value >= 0.0f && value >= _start)
+                    _end = value;
+                else
+                    // End should never be less than start
+                    _end = _start;
+            }
+        }
+    }
+}
