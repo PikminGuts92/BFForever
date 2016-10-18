e2ce76218a44738d9aea3f0e083b9ffc2aaaedd6
PikminGuts92 <PikminGuts92@gmail.com>
2016-10-17 22:38:50 -0400

    Added parameter specifying relative input path in LoadRiffFile function.

--- BFForever/SongManager.cs
+++ BFForever/SongManager.cs
@@ -15,6 +15,10 @@ namespace BFForever
         private Catalog2 _catalog2;
         private PackageDef _packageDef;
 
+        /// <summary>
+        /// Loads and manages Index2, Catalog2, PackageDef objects
+        /// </summary>
+        /// <param name="workingDirectory"></param>
         public SongManager(string workingDirectory)
         {
             // Returns if directory doesn't exist
@@ -41,11 +45,24 @@ namespace BFForever
             }
         }
 
-        public RiffFile LoadRiffFile(string relativePath)
+        /// <summary>
+        /// Loads single .rif file
+        /// <para>Absolute Path: c:\bandfuse_dlc\songs\adaytoremember\thedownfallofusall\fused.rif</para>
+        /// <para>Relative Path: songs\adaytoremember\thedownfallofusall\fused.rif</para>
+        /// </summary>
+        /// <param name="rifPath">File path</param>
+        /// <param name="relative">Path relative to SongManager directory?</param>
+        /// <returns>Riff File</returns>
+        public RiffFile LoadRiffFile(string rifPath, bool relative = true)
         {
             // Opens single rif file
             RiffFile rif = new RiffFile();
-            rif.Import(_workingDirectory + "\\" + relativePath);
+
+            if (relative)
+                // Combines working directory + input file path
+                rif.Import(Path.Combine(_workingDirectory, rifPath));
+            else
+                rif.Import(rifPath);
 
             return rif;
         }
