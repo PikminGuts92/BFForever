77d4990b1a728dbdc102d717775c54470600a570
PikminGuts92 <PikminGuts92@gmail.com>
2016-11-03 14:21:51 -0400

    Created IData interface.

---
+++ BFForever/Riff/IData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BFForever.Riff
+{
+    public interface IData
+    {
+        /// <summary>
+        /// Returns every object in data structure (not including header data)
+        /// </summary>
+        /// <returns>Objects</returns>
+        IReadOnlyCollection<object> GetObjects();
+    }
+}
