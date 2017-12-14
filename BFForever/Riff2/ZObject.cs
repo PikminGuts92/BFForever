182656750cb0254d3f93ee7b5db5701d90277cfc
PikminGuts92 <PikminGuts92@gmail.com>
2017-12-13 21:43:02 -0500

    Started writing out new classes.

---
+++ BFForever/Riff2/ZObject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BFForever.Riff2
+{
+    public abstract class ZObject
+    {
+    }
+}
