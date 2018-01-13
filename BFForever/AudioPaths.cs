867f4835679ed7473ae4f213abc0aa49616c3d03
PikminGuts92 <PikminGuts92@gmail.com>
2018-01-13 00:18:49 -0500

    Started building out template for custom songs

---
+++ BFForever/AudioPaths.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BFForever
+{
+    public class AudioPaths
+    {
+        public string Backing { get; set; }
+        public string Bass { get; set; }
+        public string Drums { get; set; }
+        public string LeadGuitar { get; set; }
+        public string RhythmGuitar { get; set; }
+        public string Vox { get; set; }
+    }
+}
---
+++ BFForever/FusedSong.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace BFForever
+{
+    public class FusedSong
+    {
+        public string Identifier { get; set; }
+        public string Author { get; set; }
+
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public string Description { get; set; }
+
+        public float SongLength { get; set; }
+        public float GuitarIntensity { get; set; }
+        public float BassIntensity { get; set; }
+        public float VoxIntensity { get; set; }
+        
+        public int Year { get; set; }
+
+        // External Files
+        public string TabPath { get; set; }
+        public string TexturePath { get; set; }
+        public string VideoPath { get; set; }
+
+        public AudioPaths AudioPaths { get; set; } = new AudioPaths();
+
+        public void Export(string path)
+        {
+            string json = JsonConvert.SerializeObject(this, Formatting.Indented);
+            File.WriteAllText(path, json);
+        }
+    }
+}
