2dd24700e91396a106d4560eaae640748b535d33
PikminGuts92 <PikminGuts92@gmail.com>
2018-05-28 00:12:29 -0400

    Created hash mapping classes

---
+++ BFForever/AudioHashMappings.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace BFForever
+{
+    public class AudioHashMappings
+    {
+        public HashMapping Preview { get; set; }
+        public HashMapping Backing { get; set; }
+        public HashMapping Bass { get; set; }
+        public HashMapping Drums { get; set; }
+        public HashMapping LeadGuitar { get; set; }
+        public HashMapping RhythmGuitar { get; set; }
+        public HashMapping Vox { get; set; }
+
+        public void Export(string path)
+        {
+            JsonSerializerSettings settings = new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore,
+                Formatting = Formatting.Indented
+            };
+
+            string json = JsonConvert.SerializeObject(this, settings);
+            File.WriteAllText(path, json);
+        }
+
+        public static HashMapping Import(string path)
+        {
+            string json = File.ReadAllText(path);
+            return JsonConvert.DeserializeObject<HashMapping>(json);
+        }
+    }
+}
---
+++ BFForever/HashMapping.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BFForever
+{
+    public class HashMapping
+    {
+        private static CRC64 _crc = new CRC64();
+
+        public ulong Input { get; set; }
+        public ulong Output { get; set; }
+
+        public static ulong ComputeHashFromFile(string path) =>
+            (!File.Exists(path)) ? 0 : _crc.Compute(File.ReadAllBytes(path));
+
+        public static HashMapping CreateMapping(string inputPath, string outputPath)
+        {
+            HashMapping map = new HashMapping();
+            map.Input = ComputeHashFromFile(inputPath);
+            map.Output = ComputeHashFromFile(outputPath);
+            return map;
+        }
+    }
+}
