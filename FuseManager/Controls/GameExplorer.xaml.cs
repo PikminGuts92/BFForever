6068b1a370474700b057911ba329cd3777549bf7
PikminGuts92 <PikminGuts92@gmail.com>
2020-03-02 22:26:02 -0500

    Added avalonia project

---
+++ FuseManager/Controls/GameExplorer.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace FuseManager.Controls
+{
+    public class GameExplorer : UserControl
+    {
+        public GameExplorer()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
